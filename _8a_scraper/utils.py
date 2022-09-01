347b321a806e7c6cd72663860084ed54259d57ef
Mieszko <mieszek2@live.com>
2022-09-01 14:55:43 +0200

    add utils

--- _8a_scraper/utils.py
+++ _8a_scraper/utils.py
@@ -1,10 +1,11 @@
 import os
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 def login():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
+    #chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument(
@@ -13,8 +14,10 @@ def login():
     driver = webdriver.Chrome(
         executable_path=ChromeDriverManager().install(), options=chrome_options)
     driver.get('https://www.8a.nu/')
+    time.sleep(3)
     driver.find_element_by_link_text('Log in').click()
     driver.find_element_by_id('username').send_keys(os.getenv('_8A_USERNAME'))
     driver.find_element_by_id('password').send_keys(os.getenv('_8A_PASSWORD'))
     driver.find_element_by_id('kc-login').submit()
+    time.sleep(0.5)
     return driver
