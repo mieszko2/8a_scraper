576c26cbd37fa48fcfc491970682f190dcc0c6f5
Mieszko <mieszek2@live.com>
2022-09-04 14:32:02 +0200

    remove redundant prints

--- _8a_scraper/sectors.py
+++ _8a_scraper/sectors.py
@@ -37,7 +37,6 @@ def get_route_ascents(driver, category, country, crag, sector, route):
         page_data = json.loads(element)
         ascents.extend(page_data['items'])
         page_index += 1
-        print(page_index)
         if not page_data['pagination']['hasNext']:
             break
     return pd.DataFrame(ascents)
@@ -57,8 +56,6 @@ def get_crag_ascents(driver, category, country, crag, min_number_of_ascents):
     routes_links = get_links_to_routes_from_a_crag(driver, category, country, crag, min_number_of_ascents)
     crag_ascents = pd.DataFrame()
     for link in routes_links:
-        print('sciagam dane dla drogi')
-        print(link)
         route_ascents = get_route_ascents_from_link(driver, link)[['date']]
         route_ascents['route_name'] = get_route_name_from_route_link(driver, link)
         route_ascents['sector_name'] = get_sector_name_from_route_link(driver, link)
