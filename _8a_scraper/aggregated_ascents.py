90a7b9be371d6b570da34f4f228c336170645075
Mieszko <mieszek2@live.com>
2022-09-05 23:58:03 +0200

    add tests

--- _8a_scraper/aggregated_ascents.py
+++ _8a_scraper/aggregated_ascents.py
@@ -58,9 +58,11 @@ def get_crag_ascents(driver, category, country, crag, min_number_of_ascents,
     routes_links = get_links_to_routes_from_a_crag(driver, category, country, crag, min_number_of_ascents)
     crag_ascents = pd.DataFrame()
     for link in routes_links:
-        route_ascents = get_route_ascents_from_link(driver, link, min_date)[['date']]
-        route_ascents['route_name'] = get_route_name_from_route_link(driver, link)
-        route_ascents['sector_name'] = get_sector_name_from_route_link(driver, link)
-        crag_ascents = pd.concat([crag_ascents, route_ascents])
+        route_ascents = get_route_ascents_from_link(driver, link, min_date)
+        if not route_ascents.empty:
+            route_ascents = route_ascents[['date']]
+            route_ascents['route_name'] = get_route_name_from_route_link(driver, link)
+            route_ascents['sector_name'] = get_sector_name_from_route_link(driver, link)
+            crag_ascents = pd.concat([crag_ascents, route_ascents])
     crag_ascents.reset_index(drop=True, inplace=True)
     return crag_ascents
